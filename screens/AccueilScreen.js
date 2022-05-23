860c390e5dd04f5b417f8990698112564e919ebe
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2022-05-23 17:39:08 +0200

    US20 et US12 logo et harmonisation du front

--- screens/AccueilScreen.js
+++ screens/AccueilScreen.js
@@ -1,45 +1,27 @@
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  Image,
-  Dimensions,
-} from "react-native";
-
-const windowWidth = Dimensions.get("window").width;
-const windowHeight = Dimensions.get("window").height;
-import monjson from "../jsonModels/url.json";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
 
 export default function AccueilScreen(props) {
   return (
     <View style={styles.container}>
-      <Text h1>Bienvenue sur TOUTATOI</Text>
-
-      <Image
-        style={styles.image}
-        source={require("../assets/ImageAccueilTest.png")}
-      />
-
-      <View>
+      <Text style={styles.h1}>Bienvenue sur</Text>
+      <Image source={require("../assets/logoTest.png")}></Image>
+      <View style={styles.buttonDisplay}>
         <TouchableOpacity
-          style={styles.button1}
+          style={styles.button2}
           onPress={() => props.navigation.navigate("KidProfil")}
         >
-          <Text style={styles.fonts} flex-start>
-            C'est parti !
-          </Text>
+          <Text style={styles.fonts}>C'est parti !</Text>
         </TouchableOpacity>
 
         <TouchableOpacity
-          style={styles.button2}
+          style={styles.button1}
           onPress={() => props.navigation.navigate("SignIn")}
         >
           <Text style={styles.fonts}>J'ai un compte</Text>
         </TouchableOpacity>
         <TouchableOpacity
-          style={styles.button2}
+          style={styles.button1}
           onPress={() => props.navigation.navigate("SignIn")}
         >
           <Text style={styles.fonts}>Je suis invité</Text>
@@ -53,36 +35,117 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "space-around",
+    backgroundColor: "white",
+    justifyContent: "space-between",
+    paddingTop: 20,
+    paddingBottom: 20,
   },
-  buttonDisplay: {
-    flexDirection: "row",
-    flexWrap: "wrap",
+  h1: {
+    fontSize: 30,
+    fontWeight: "bold",
   },
   button1: {
+    width: 190,
     color: "white",
     backgroundColor: "#49A078",
-    width: 200,
-    padding: 4,
+    padding: 15,
     marginRight: 10,
     marginBottom: 10,
+    borderRadius: 20,
   },
   button2: {
+    width: 190,
     color: "white",
     backgroundColor: "#FFC9B9",
-    width: 200,
-    padding: 4,
+    padding: 15,
     marginRight: 10,
     marginBottom: 10,
+    borderRadius: 20,
   },
+
   fonts: {
     color: "white",
-    marginTop: 10,
-    marginBottom: 8,
     textAlign: "center",
-  },
-  image: {
-    width: windowWidth - windowWidth / 2,
-    height: windowHeight - windowHeight / 2,
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
+
+// const windowWidth = Dimensions.get("window").width;
+// const windowHeight = Dimensions.get("window").height;
+// import monjson from "../jsonModels/url.json";
+
+// export default function AccueilScreen(props) {
+//   return (
+//     <View style={styles.container}>
+//       <Text h1>Bienvenue sur TOUTATOI</Text>
+
+//       <Image
+//         style={styles.image}
+//         source={require("../assets/ImageAccueilTest.png")}
+//       />
+
+//       <View>
+//         <TouchableOpacity
+//           style={styles.button1}
+//           onPress={() => props.navigation.navigate("KidProfil")}
+//         >
+//           <Text style={styles.fonts} flex-start>
+//             C'est parti !
+//           </Text>
+//         </TouchableOpacity>
+
+//         <TouchableOpacity
+//           style={styles.button2}
+//           onPress={() => props.navigation.navigate("SignIn")}
+//         >
+//           <Text style={styles.fonts}>J'ai un compte</Text>
+//         </TouchableOpacity>
+//         <TouchableOpacity
+//           style={styles.button2}
+//           onPress={() => props.navigation.navigate("SignIn")}
+//         >
+//           <Text style={styles.fonts}>Je suis invité</Text>
+//         </TouchableOpacity>
+//       </View>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "space-around",
+//   },
+//   buttonDisplay: {
+//     flexDirection: "row",
+//     flexWrap: "wrap",
+//   },
+//   button1: {
+//     color: "white",
+//     backgroundColor: "#49A078",
+//     width: 200,
+//     padding: 4,
+//     marginRight: 10,
+//     marginBottom: 10,
+//   },
+//   button2: {
+//     color: "white",
+//     backgroundColor: "#FFC9B9",
+//     width: 200,
+//     padding: 4,
+//     marginRight: 10,
+//     marginBottom: 10,
+//   },
+//   fonts: {
+//     color: "white",
+//     marginTop: 10,
+//     marginBottom: 8,
+//     textAlign: "center",
+//   },
+//   image: {
+//     width: windowWidth - windowWidth / 2,
+//     height: windowHeight - windowHeight / 2,
+//   },
+// });
